d932670d90e5515bb75e8780c6976486530fbf91
RahulPatil-N7 <rahul.patil.3125@gmail.com>
2022-07-17 16:27:19 +0530

    Added : welcome message.

---
+++ bridgelabz/BinaryTree/BinarySearchTreeMain.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.BinaryTree;
+
+public class BinarySearchTreeMain {
+
+	public static void main(String[] args) {
+	
+	//printing welcome message	
+	System.out.println("Welcome To Binary Search Tree Program.");
+	
+	}
+}
+
+
